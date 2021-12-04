43ab59ef2700babab911c98e9100f6141b79eae8
MNTJN <tijani.amine@gmail.com>
2021-12-04 11:12:51 +0100

    Fixed computerPlay function

--- main.js
+++ main.js
@@ -1,30 +1,10 @@
 'use strict';
 
-let translateNumberToMove = (numberToTranslate) => {
-    if(numberToTranslate == 1){
-        return "ROCK";
-    }else if(numberToTranslate == 2){
-        return "PAPER";
-    }else if(numberToTranslate == 3){
-        return "SCISSORS";
-    }
-    else{
-        return 0;
-    }
+const moves = ["ROCK","PAPER","SCISSORS"];
+
+let computerPlay = () => { 
+    return moves[Math.floor(Math.random() * moves.length)]; 
 };
 
-let computerPlay = () => {
-    let options = [1, 2, 3];
-    return translateNumberToMove(options[Math.floor(Math.random() * options.length)]);
-};
 
-let playerSelection = () => {
-    let selection = 0;
-    while(selection < 1 || selection > 3){
-        selection = prompt("### CHOOSE YOUR MOVE ### \n ROCK : 1 \n PAPER : 2 \n SCISSORS : 3");
-        if(selection < 1 || selection > 3){
-            alert("ROCK : 1 \n PAPER : 2 \n SCISSORS : 3");
-        }
-    }
-    return translateNumberToMove(selection);
-};
+console.log(computerPlay());
