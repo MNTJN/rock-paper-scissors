9a74fc3b9894b9246b36ef273683cf757de05032
MNTJN <tijani.amine@gmail.com>
2021-12-04 17:16:59 +0100

    Added GUI

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 'use strict';
+/* DOM elements */
+let gameContainer = document.getElementsByClassName("active-game-container")[0];
+let iconsContainer = document.getElementsByClassName("icons-container")[0];
+let rulesContainer = document.getElementsByClassName("rules")[0];
+let scoreContainer = document.getElementsByClassName("score-container")[0];
+let scoreDisplay = document.getElementsByClassName("score-display")[0];
+let resultDisplay = document.getElementsByClassName("result-display")[0];
+let replayButton = document.getElementsByClassName("replay-button")[0];
 
 /* Possible moves. Changing the moves names here will affect the whole game.
 ###Logical rules are as follows : ### 
@@ -7,36 +15,52 @@ moves[1] beats moves[0]
 moves[2] beats moves[1] */ 
 const moves = ["ROCK","PAPER","SCISSORS"];
 
+/* Initial score */
 let computerScore = 0;
 let playerScore = 0;
-let playGame = 1; // The game will only start if value is 1.
+
+/* This function starts the game by reinitializing the scores to 0 and the display of all the elements */
+let startGame = () => {
+    playerScore = 0;
+    computerScore = 0;
+    gameContainer.style.display = "block";
+    iconsContainer.style.display = "flex"
+    rulesContainer.style.display = "none";
+    scoreDisplay.textContent = "";
+    resultDisplay.textContent = "";
+    resultDisplay.style.display = "none";
+    replayButton.style.display = "none";
+};
 
 /* Displays the current score */
-let showScore = () => {
-    return `SCORE \n You : ${playerScore} | Computer : ${computerScore}`;
+let getScore = () => {
+    return `### SCORE ### You : ${playerScore} | Computer : ${computerScore}`;
 };
+
+/* Checks if any player has reached 5 points. If true, hides the icons so it is impossible to keep playing */
+let checkResult = () => {
+    if(playerScore == 5 || computerScore == 5){
+        iconsContainer.style.display = "none";
+        replayButton.style.display = "block";
+        resultDisplay.style.display = "block";
+        if(playerScore == 5){
+            resultDisplay.textContent = `Congratulations ! You win \\o/`;
+        }
+        else if(computerScore == 5){
+            resultDisplay.textContent = `Too bad ! You lose...`;
+        }
+    }
+}
 
 /* Determines the computer's move */
 let computerPlay = () => { 
     return moves[Math.floor(Math.random() * moves.length)]; 
 };
 
-/* Prompts the player to choose his move */
-let playerSelection = () => {
-    let playerMove = '';
-    while(playerMove != moves[0] && playerMove != moves[1] && playerMove != moves[2]){
-        playerMove = prompt(`### CHOOSE YOUR MOVE ### \n TYPE ${moves[0]}, ${moves[1]}, OR ${moves[2]} :`).toUpperCase();
-        if(playerMove != moves[0] && playerMove != moves[1] && playerMove != moves[2]){
-            alert(`Only ${moves[0]}, ${moves[1]}, or ${moves[2]} accepted.`);
-        }
-    }
-    return playerMove;
-};
-
-/* Launches a single round. Takes 2 parameters, the moves of the 2 players, then compares them. Announces the winner of the round and shows the score. */
+/* Launches a single round. Takes 2 parameters (the moves of the 2 players), then compares them. Announces the winner of the round and shows the score. */
 let playRound = (computerMove, playerMove) => {
     if(computerMove == playerMove){
-        alert(`It's a tie ! You both chose ${playerMove} \n ${showScore()}`);
+        scoreDisplay.textContent = `It's a tie ! You both chose ${playerMove} ${getScore()}`;
     }
     else{
         switch(true){
@@ -44,42 +68,14 @@ let playRound = (computerMove, playerMove) => {
             case (playerMove == moves[1] && computerMove == moves[0]) :
             case (playerMove == moves[2] && computerMove == moves[1]) :
                 playerScore++; // Player scores 1 point.
-                alert(`You win ! ${playerMove} beats ${computerMove} ! \n ${showScore()}`);
+                scoreDisplay.textContent = `You win this round ! ${playerMove} beats ${computerMove} ! ${getScore()}` ;
+                checkResult();
             break;
             default :
                 computerScore++; // Computer scores 1 point.
-                alert(`You lose ! ${computerMove} beats ${playerMove} ! \n ${showScore()}`);
+                scoreDisplay.textContent = `You lose this round ! ${computerMove} beats ${playerMove} ! ${getScore()}`;
+                checkResult();
             break;
         }
     }
 };
-
-/* Start of game loop */
-while(playGame == 1)
-{   
-    /* Reset of the score at the start of the game */
-    playerScore = 0;
-    computerScore = 0;
-
-    /* Runs while nobody has reached a score of 5 */
-    while(computerScore != 5 && playerScore != 5){
-        playRound(computerPlay(), playerSelection());
-    }
-
-    if(computerScore == 5){
-        alert(`You lost ! `)
-    }
-    else{
-        alert(`Congratulations ! You win !`);
-    }
-
-    playGame = 0; // Set to 0 to prevent the game from restarting
-
-    /* Prompts the player to choose to play again or quit */
-    while(playGame != 1 && playGame != 2){
-        playGame = parseInt(prompt(`Play again ? \n 1: YES | 2: NO`));
-        if(playGame != 1 && playGame != 2){
-            alert(`Type 1 to play again or 2 to quit`);
-        }
-    }
-} // End of the game loop
