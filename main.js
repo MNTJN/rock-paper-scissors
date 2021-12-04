cf012ba4560ed3c071106e695c3f94ff49ce90fb
MNTJN <tijani.amine@gmail.com>
2021-12-04 11:40:10 +0100

    Fixed playerSelection function

--- main.js
+++ main.js
@@ -6,5 +6,13 @@ let computerPlay = () => {
     return moves[Math.floor(Math.random() * moves.length)]; 
 };
 
-
-console.log(computerPlay());
+let playerSelection = () => {
+    let playerMove = '';
+    while(playerMove != moves[0] && playerMove != moves[1] && playerMove != moves[2]){
+        playerMove = prompt(`### CHOOSE YOUR MOVE ### \n TYPE ${moves[0]}, ${moves[1]}, OR ${moves[2]} :`).toUpperCase();
+        if(playerMove != moves[0] && playerMove != moves[1] && playerMove != moves[2]){
+            alert(`Only ${moves[0]}, ${moves[1]}, or ${moves[2]} accepted.`);
+        }
+    }
+    return playerMove;
+};
