afb3030b2d5d65ad07475b99e02ad2f6f77439ba
MNTJN <tijani.amine@gmail.com>
2021-12-10 10:58:48 +0100

    new UI

--- main.js
+++ main.js
@@ -1,130 +1,181 @@
 'use strict';
-/* DOM elements */
-let gameContainer = document.getElementsByClassName("active-game-container")[0];
-let iconsContainer = document.getElementsByClassName("icons-container")[0];
-let rulesContainer = document.getElementsByClassName("rules")[0];
-let scoreContainer = document.getElementsByClassName("score-container")[0];
-let scoreDisplay = document.getElementsByClassName("score-display")[0];
-let resultDisplay = document.getElementsByClassName("result-display")[0];
-let replayButton = document.getElementsByClassName("replay-button")[0];
-let movesDisplay = document.getElementsByClassName("moves")[0];
-let playerIcon = document.getElementsByClassName("player-move-icon")[0];
-let computerIcon = document.getElementsByClassName("computer-move-icon")[0];
-let roundCounter = document.getElementsByClassName("round-counter")[0];
+/* DOM Elements */
+const startButton = document.querySelector('.start-button');
+const restartButton = document.querySelector('.replay-button');
+const rulesContainer = document.querySelector('.rules-container');
+const scoreContainer = document.querySelector('.score-container');
+const resultsContainer = document.querySelector('.results-container');
+const iconsContainer = document.querySelector('.icons-container');
+const history = document.querySelector('.game-history-container');
+const roundCounter = document.querySelector('.round-counter');
+const icons = document.querySelectorAll('.icon i');
 
-/* Possible moves. Changing the moves names here will affect the whole game.
-###Logical rules are as follows : ### 
-moves[0] beats moves[2]
-moves[1] beats moves[0]
-moves[2] beats moves[1] */ 
-const moves = ["ROCK","PAPER","SCISSORS"];
-const playerMovesIcons = ["fa-hand-rock", "fa-hand-paper", "fa-hand-scissors"];
+/* Possible moves. 
+### Change names here to apply to the whole game ### */
+const moves = ['ROCK','PAPER','SCISSORS'];
 
-/* Variables for storing the last played move of each player */
-let computerSelection = '';
-let playerSelection = '';
+/* Moves icons */
+const fontAwesomeIcons = ['fas fa-hand-rock','fas fa-hand-paper','fas fa-hand-scissors'];
 
-/* Initial score */
-let computerScore = 0;
 let playerScore = 0;
-
-/* Round counter */
+let computerScore = 0;
 let roundCount = 0;
+let playerSelection = '';
+let computerSelection = '';
 
-/* This function starts the game by reinitializing the scores to 0 and the display of all the elements */
-let startGame = () => {
+const loadPage = () => {
+    const pageTitle = document.querySelector('.game-title');
+    pageTitle.textContent = `${moves[0]}, ${moves[1]}, ${moves[2]}`;
+    const rules = document.querySelector('.rules');
+    rules.textContent = `${moves[0]} beats ${moves[2]} | ${moves[1]} beats ${moves[0]} | ${moves[2]} beats ${moves[1]}`;
+};
+loadPage();
+
+const insertIcons = () => {
+    let i = 0;
+    iconsContainer.style.visibility = 'initial';
+    icons.forEach(icon => {
+        icon.setAttribute('class', fontAwesomeIcons[i]);
+        icon.setAttribute('move', moves[i]);
+        i++;
+    });
+};
+
+let computerPlay = () => { 
+    computerSelection = moves[Math.floor(Math.random() * moves.length)];
+}
+
+const setIconsEventListeners = () => {
+    icons.forEach(icon => {
+        icon.addEventListener('click', function(){
+            this.classList.add('clicked');
+            for(let i = 0 ; i < this.attributes.length ; i++){
+                if(this.attributes[i].name === 'move'){
+                    playerSelection = this.attributes[i].value;
+                }
+            }
+            playRound();
+        });
+        icon.addEventListener('transitionend', function(){
+            this.classList.remove('clicked');
+        });
+    });    
+}
+
+/* Determines the winner of each round, updates the score and the history. */
+const compareMoves = () => {
+    let playerIconToDisplay = '';
+    let computerIconToDisplay = '';
+    for(let i = 0 ; i < fontAwesomeIcons.length ; i++){
+        if(playerSelection == moves[i]){
+            playerIconToDisplay = fontAwesomeIcons[i];
+        }
+        if(computerSelection == moves[i]){
+            computerIconToDisplay = fontAwesomeIcons[i];
+        }
+    }
+    const newEntry = document.createElement('div');
+    newEntry.setAttribute('class', 'game-history-element');
+    const historyRound = document.createElement('p');
+    historyRound.setAttribute('class', 'game-history-round-display');
+    const playerDisplayedIcon = document.createElement('i');
+    const computerDisplayedIcon = document.createElement('i');
+    const iconsDiv = document.createElement('div');
+    iconsDiv.setAttribute('class','game-history-icons-display');
+    const playerIconParagraph = document.createElement('p');
+    playerIconParagraph.textContent = 'YOU';
+    const computerIconParagraph = document.createElement('p');
+    computerIconParagraph.textContent = 'COMPUTER';
+
+    if(computerSelection == playerSelection){
+        historyRound.textContent = `Round ${roundCount} : TIE ! `;
+    }
+    else{
+        switch(true){
+            case (playerSelection == moves[0] && computerSelection == moves[2]) :
+            case (playerSelection == moves[1] && computerSelection == moves[0]) :
+            case (playerSelection == moves[2] && computerSelection == moves[1]) :
+                playerScore++;
+                historyRound.textContent = `Round ${roundCount} : WIN !`;
+            break;
+            default :
+                computerScore++;
+                historyRound.textContent = `Round ${roundCount} : LOSE !`;
+            break;
+        }
+    }
+    newEntry.appendChild(historyRound);
+    playerDisplayedIcon.setAttribute('class',playerIconToDisplay);
+    computerDisplayedIcon.setAttribute('class', computerIconToDisplay);
+    playerIconParagraph.appendChild(playerDisplayedIcon);
+    computerIconParagraph.appendChild(computerDisplayedIcon);
+    iconsDiv.appendChild(playerIconParagraph);
+    iconsDiv.appendChild(computerIconParagraph);
+    newEntry.appendChild(iconsDiv);
+
+    if(!document.querySelector('.game-history-element')){
+        history.appendChild(newEntry);
+    }
+    else{
+        history.insertBefore(newEntry, document.querySelector('.game-history-element'));
+    }
+}
+
+const updateRound = () => {
+    roundCount ++;
+    roundCounter.textContent = `Round : ${roundCount}`;
+}
+
+const updateScores = () => {
+    const playerScoreDisplay = document.querySelector('.player-score-display');
+    const computerScoreDisplay = document.querySelector('.computer-score-display');
+    playerScoreDisplay.textContent = `${playerScore}`;
+    computerScoreDisplay.textContent = `${computerScore}`;
+}
+
+const resetScoreAndRounds = () => {
     playerScore = 0;
     computerScore = 0;
-    roundCount = 0;
-    displayRound();
-    gameContainer.style.display = "block";
-    iconsContainer.style.display = "flex";
-    rulesContainer.style.display = "none";
-    scoreDisplay.textContent = "";
-    resultDisplay.textContent = "";
-    movesDisplay.style.display = "flex";
-    resultDisplay.style.display = "none";
-    replayButton.style.display = "none";
-    removeMovesIcons();
+    roundCount = 0;  
 };
 
-/* Returns formatted current score */
-let getScore = () => {
-    return `### SCORE ### You : ${playerScore} | Computer : ${computerScore}`;
-};
-
-/* Checks if any player has reached 5 points. If true, hides the icons so it is impossible to keep playing */
-let checkResult = () => {
+const checkResult = () => {
+    const resultsDisplay = document.querySelector('.results');
     if(playerScore == 5 || computerScore == 5){
-        iconsContainer.style.display = "none";
-        replayButton.style.display = "block";
-        movesDisplay.style.display = "none";
-        resultDisplay.style.display = "block";
+        iconsContainer.style.visibility = 'hidden';
+        resultsContainer.style.display = 'block';
         if(playerScore == 5){
-            resultDisplay.textContent = `Congratulations ! You win \\o/`;
+            resultsDisplay.textContent = `Congrats ! you WIN \\o/`;
         }
         else if(computerScore == 5){
-            resultDisplay.textContent = `Too bad ! You lose...`;
+            resultsDisplay.textContent = `Too bad ! you LOSE ...`;
         }
     }
 }
 
-/* Determines the computer's move */
-let computerPlay = () => {
-    computerSelection = moves[Math.floor(Math.random() * moves.length)]; 
-    return computerSelection;
+const playRound = () => {
+    computerPlay();
+    updateRound();
+    compareMoves();
+    updateScores();
+    checkResult();
+}
+
+const startGame = () => {
+    updateScores();
+    rulesContainer.style.display = 'none';
+    scoreContainer.style.display = 'block';
+    resultsContainer.style.display = 'none';
+    insertIcons();
+    setIconsEventListeners();
 };
 
-/* Removes the icon of the previous move */
-let removeMovesIcons = () => {
-    playerIcon.classList.remove(playerMovesIcons[0], playerMovesIcons[1], playerMovesIcons[2]);
-    computerIcon.classList.remove(playerMovesIcons[0], playerMovesIcons[1], playerMovesIcons[2]);
-};
-
-let addRound = () => {
-    roundCount++;
-    displayRound();
-};
-
-let displayRound = () => {
-    roundCounter.textContent = `Current round : ${roundCount}`;
-};
-
-/* Displays the current move of each player */
-let showMoves = () => {
-    removeMovesIcons();
-    for(let i = 0 ; i < moves.length ; i++){
-        if(moves[i] == playerSelection){
-            playerIcon.classList.add(playerMovesIcons[i]);
-        }
-        if(moves[i] == computerSelection){
-            computerIcon.classList.add(playerMovesIcons[i]);
-        }
-    }
-};
-
-/* Launches a single round. Takes 2 parameters (the moves of the 2 players), then compares them. Announces the winner of the round and shows the score. */
-let playRound = (computerMove, playerMove) => {
-    addRound();
-    playerSelection = playerMove;
-    showMoves();
-    if(computerMove == playerMove){
-        scoreDisplay.textContent = `It's a tie ! You both chose ${playerMove} ${getScore()}`;
-    }
-    else{
-        switch(true){
-            case (playerMove == moves[0] && computerMove == moves[2]) :
-            case (playerMove == moves[1] && computerMove == moves[0]) :
-            case (playerMove == moves[2] && computerMove == moves[1]) :
-                playerScore++; // Player scores 1 point.
-                scoreDisplay.textContent = `You win this round ! ${playerMove} beats ${computerMove} ! ${getScore()}` ;
-                checkResult();
-            break;
-            default :
-                computerScore++; // Computer scores 1 point.
-                scoreDisplay.textContent = `You lose this round ! ${computerMove} beats ${playerMove} ! ${getScore()}`;
-                checkResult();
-            break;
-        }
-    }
-};
+startButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', function(){
+    resetScoreAndRounds();
+    updateScores();
+    roundCounter.textContent = 'Round : 0';
+    resultsContainer.style.display = 'none';
+    iconsContainer.style.visibility = 'initial';
+    history.textContent = '';
+});
